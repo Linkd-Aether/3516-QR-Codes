b590adc879651b3a296420ea1af2fcb6ec65425e
naomitr <52180609+naomitr@users.noreply.github.com>
2022-11-01 22:32:48 -0400

    added class notes

--- QRServer.cpp
+++ QRServer.cpp
@@ -1,56 +1,95 @@
-#include <stdio.h>      /* for printf() and fprintf() */
+#include <stdio.h>          /* for printf() and fprintf() */
 #include <sys/socket.h> /* for socket(), bind(), and connect() */
-#include <arpa/inet.h>  /* for sockaddr_in and inet_ntoa() */
-#include <stdlib.h>     /* for atoi() and exit() */
-#include <string.h>     /* for memset() */
-#include <unistd.h>     /* for close() */
+#include <arpa/inet.h>   /* for sockaddr_in and inet_ntoa() */
+#include <stdlib.h>       /* for atoi() and exit() */
+#include <string.h>       /* for memset() */
+#include <unistd.h>      /* for close() */
 
-#define MAXPENDING 5                   /* Maximum outstanding connection requests */
-void DieWithError(char *errorMessage); /* Error handling function */
-void HandleTCPClient(int clntSocket);  /* TCP client handling function */
+#define MAXPENDING 5             /* Maximum outstanding connection requests */
+//need to be changed so can't be define
+void DieWithError(char *errorMessage);     /* Error handling function */
+//also communicate with client, diff errors so if statements
+void HandleTCPClient(int clntSocket);       /* TCP client handling function */
+//logic of decoding QR code and sending it back
+
+//while loop with geopt(), check for what is specified, moves to end until null is returned
 
 int main(int argc, char *argv[])
-{
-    int servSock;                    /*Socket descriptor for server */
-    int clntSock;                    /* Socket descriptor for client */
-    struct sockaddr_in echoServAddr; /* Local address */
-    struct sockaddr_in echoClntAddr; /* Client address */
-    unsigned short echoServPort;     /* Server port */
-    unsigned int clntLen;            /* Length of client address data structure */
+{    int servSock;                                   /*Socket descriptor for server */
+     int clntSock;                                   /* Socket descriptor for client */
+     struct sockaddr_in echoServAddr; /* Local address */
+     struct sockaddr_in echoClntAddr; /* Client address */
+     unsigned short echoServPort;        /* Server port */
+     unsigned int clntLen;                     /* Length of client address data structure */ 
 
-    if (argc != 2) /* Test for correct number of arguments */
-    {
-        fprintf(stderr, "Usage:  %s <Server Port>\n", argv[0]);
-        exit(1);
-    }
-    echoServPort = atoi(argv[1]); /* First arg:  local port */
+      if (argc != 4)     /* Test for correct number of arguments */
+     { 
+           fprintf(stderr, "Usage:  %s <Server Port>\n", argv[0]);
+            exit(1);   
+     }
+     echoServPort = atoi(argv[1]);        /* First arg:  local port */
+	 //change to correct port instead of argv[1]
+	 
     /* Create socket for incoming connections */
-    if ((servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
-        DieWithError("socket() failed");
-    /* Construct local address structure */
-    memset(&echoServAddr, 0, sizeof(echoServAddr));   /* Zero out structure */
-    echoServAddr.sin_family = AF_INET;                /* Internet address family */
-    echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
-    echoServAddr.sin_port = htons(echoServPort);      /* Local port */
+      if ((servSock = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) 
+     	DieWithError("socket() failed");   
 
+/* Construct local address structure */
+    memset(&echoServAddr, 0, sizeof(echoServAddr));         /* Zero out structure */
+    echoServAddr.sin_family         = AF_INET;                      /* Internet address family */
+    echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
+    echoServAddr.sin_port             = htons(echoServPort);     /* Local port */
+    
     /* Bind to the local address */
-    if (bind(servSock, (struct sockaddr *)&echoServAddr, sizeof(echoServAddr)) < 0)
-        DieWithError("bind() failed");
+   if (bind (servSock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+	DieWithError("bind() failed");
 
     /* Mark the socket so it will listen for incoming connections */
-    if (listen(servSock, MAXPENDING) < 0)
-        DieWithError("listen() failed");
+    if (listen (servSock, MAXPENDING) < 0)
+    	DieWithError("listen() failed");
+	
+	/* Construct local address structure */
+    memset(&echoServAddr, 0, sizeof(echoServAddr));         /* Zero out structure */
+    echoServAddr.sin_family         = AF_INET;                      /* Internet address family */
+    echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
+    echoServAddr.sin_port             = htons(echoServPort);     /* Local port */
+    
+    /* Bind to the local address */
+   if (bind (servSock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+	DieWithError("bind() failed");
 
-    for (;;) /* Run forever */
+    /* Mark the socket so it will listen for incoming connections */
+    if (listen (servSock, MAXPENDING) < 0)
+    	DieWithError("listen() failed");
+	for (;;) /* Run forever */
     {
         /* Set the size of the in-out parameter */
-        clntLen = sizeof(echoClntAddr); /* Wait for a client to connect */
-        if ((clntSock = accept(servSock, (struct sockaddr *)&echoClntAddr, &clntLen)) < 0)
-            DieWithError("accept() failed");
+        clntLen = sizeof(echoClntAddr);        /* Wait for a client to connect */
+        if ((clntSock = accept (servSock, (struct sockaddr *) &echoClntAddr, &clntLen))
+             < 0)            	DieWithError("accept() failed");
 
         /* clntSock is connected to a client! */
         printf("Handling client %s\n", inet_ntoa(echoClntAddr.sin_addr));
         HandleTCPClient(clntSock);
-    }
-    /* NOT REACHED */
-}
+     }
+     /* NOT REACHED */
+    } 
+	
+	//what else are we doing?
+	/*
+		log everything that's happening
+		server gets a length of what they send
+		create a buffer to hold the length of what they send
+		loop until it receives all packets
+			receive function, in socket programing
+			keep track of what is received 
+			
+			
+		read from binary file
+			fopen("file.txt", rb);
+		how big a transmitted file is
+			get it with 32 bit, need to convert because it's in network form
+			
+	
+	
+	*/
